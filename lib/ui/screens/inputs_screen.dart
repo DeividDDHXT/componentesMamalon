19511946c3a499175a95966dea1e454f25eb046c
DeividDDHXT <jestmon02@gmail.com>
2023-03-27 14:02:43 -0600

    second

--- lib/ui/screens/inputs_screen.dart
+++ lib/ui/screens/inputs_screen.dart
@@ -21,21 +21,75 @@ class _InputsScreenState extends State<InputsScreen> {
           vertical: 20.0,
         ),
         children: [
-          _createEntrada(),
+          _createEntradaNombre(),
+          _createEntradaPass(),
         ],
       ),
     );
   }
 
-  Widget _createEntrada() {
+  Widget _createEntradaNombre() {
     return TextField(
       autofocus: true,
-      textCapitalization: TextCapitalization.words,
-      decoration: InputDecoration(
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(20.0),
-        ),
+      textAlign: TextAlign.center,
+      textCapitalization: TextCapitalization.sentences,
+      style: const TextStyle(
+        color: Colors.amberAccent,
+        fontWeight: FontWeight.bold,
       ),
+      cursorColor: Color.fromARGB(255, 0, 25, 57),
+      cursorRadius: const Radius.circular(10.0),
+      cursorWidth: 5.0,
+      //maxLength: 4,
+
+      maxLines: 2,
+      decoration: InputDecoration(
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+          hintText: "Escriba su nombre",
+          labelText: "User name",
+          //helperText: "Nombre",
+          prefixIcon: const Icon(Icons.person)
+          // icon: const Icon(Icons.person),,
+          //iconColor: Colors.blueGrey,
+          ),
+      onChanged: (valor) {
+        _userName = valor;
+        print(_userName);
+      },
+    );
+  }
+
+  Widget _createEntradaPass() {
+    return TextField(
+      autofocus: true,
+      textAlign: TextAlign.center,
+      textCapitalization: TextCapitalization.sentences,
+      style: const TextStyle(
+        color: Colors.amberAccent,
+        fontWeight: FontWeight.bold,
+      ),
+      cursorColor: Colors.red,
+      cursorRadius: const Radius.circular(10.0),
+      cursorWidth: 5.0,
+      maxLength: 8,
+      obscureText: true,
+      keyboardType: TextInputType.number,
+
+      //maxLines: 2,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+        hintText: "Escriba su nombre",
+        labelText: "Password",
+
+        //helperText: "Nombre",
+        prefixIcon: const Icon(Icons.password),
+        // icon: const Icon(Icons.person),,
+        //iconColor: Colors.blueGrey,
+      ),
+      onChanged: (valor) {
+        _userName = valor;
+        print(_userName);
+      },
     );
   }
 }
